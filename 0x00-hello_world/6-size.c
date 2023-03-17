ca6df9e59a52a58f1420dab7d85015d1927beed5
Arkitet <ehistanislaus@gmail.com>
2023-03-17 11:03:54 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
  * main - starts runtime
  *
- * Return: 0 on success
+ * Return: o on success
  */
 
 int main(void)
 {
-	printf("Size of char: %lu byte(s)", sizeof(char)\n);
-	printf("size of int: %lu byte(S)", sizeof(int)\n);
-	printf("size of a long int: %lu byte(s)", sizeof(long int)\n);
-	printf("size of a long long int: %lu byte(s)", sizeof(long long int)\n);
-	printf("size of float: %lu byte(s)", sizeof(float)\n);
-	return (0):
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
