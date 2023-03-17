cf4f6bd5cf930ddd151337f0abbba6180b0dcb7d
Arkitet <ehistanislaus@gmail.com>
2023-03-17 10:27:28 -0700

    print 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - indicates entry point
+ *
+ * Return: 0if successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
