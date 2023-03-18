3c47a331cd8b5da8019ae7dc9a393412272932f6
Arkitet <ehistanislaus@gmail.com>
2023-03-17 20:09:15 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- entry point indicates start of runtime
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
