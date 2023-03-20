e9357e0d3b719443074ca7da0dc227ea12e7288e
Arkitet <ehistanislaus@gmail.com>
2023-03-19 22:40:50 -0700

    size again

--- 0x00-hello_world/.6-size.c
+++ 0x00-hello_world/.6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point for runtime
  *
  * Return: 0 on success
  */
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a long long init: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
