c04992dae0751a53c95d38b386c003d51c939a97
Arkitet <ehistanislaus@gmail.com>
2023-03-22 18:58:35 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,11 +15,11 @@ int main(void)
 	{
 	if (i == 0)
 	printf("%ld", j);
-	else if (i== 1)
+	else if (i == 1)
 	printf(", %ld", k)
 	else
 	{
-	k+= j;
+	k += j;
 	j = k - j;
 	printf(", %ld", k);
 	}
